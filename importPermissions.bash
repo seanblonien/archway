b9811e39e3b320e7ef301aad8f87cee7ed4984bb
Sean Blonien <sean_blonien@baylor.edu>
2020-01-31 13:11:11 -0600

    Commented docker files, added mongo-dev shell script

--- importPermissions.bash
+++
@@ -1,3 +0,0 @@
-mongoimport --db strapi --collection users-permissions_role --file ./roles.json;
-mongoimport --db strapi --collection users-permissions_user --file ./users.json;
-mongoimport --db strapi --collection users-permissions_permission --file ./perms.json;
