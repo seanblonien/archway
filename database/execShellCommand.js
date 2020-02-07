0b0c29cd8d6b62945c3ab34adaa082f601e55569
Sean Blonien <sean_blonien@baylor.edu>
2020-02-07 11:30:45 -0600

    Added shell command output/error handling

---
+++ database/execShellCommand.js
@@ -0,0 +1,20 @@
+const { exec } = require("child_process");
+
+// Executes a shell command and logs stdout to console if successful.
+// On error, and error will be thrown with the stderr output.
+const execShellCommand = cmd => {
+    console.log(`Executing command '${cmd}'`);
+    return new Promise((resolve, reject) => {
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                throw new Error(stderr);
+            }
+            if(stdout) console.log(stdout);
+            resolve();
+        });
+    });
+};
+
+module.exports = {
+    execShellCommand
+};
--- database/import-database.js
+++ database/import-database.js
@@ -1,13 +1,22 @@
-const { exec } = require("child_process");
+const { execShellCommand } = require('./execShellCommand');
 const path = require("path");
 
-// Delete any dump directory on the container
-exec(`docker exec database rm -r /dump`);
-// Delete the dump zip file if on container
-exec(`docker exec database rm /dump.zip`);
-// Copy the dump zip file over to the container
-exec(`docker cp ${path.resolve(__dirname, `dump.zip`)} database:/dump.zip`);
-// Unzip the dump zip into a directory
-exec(`docker exec database unzip /dump.zip -d /`);
-// Perform the data dump import of the strapi database
-exec(`docker exec database mongorestore --uri "mongodb://root:capstone@database:27017/?authSource=admin" --drop -d strapi /dump/strapi`);
+(async () => {
+    try {
+        // Delete any dump directory on the container
+        await execShellCommand(`docker exec database rm -rf /dump`);
+        // Delete the dump zip file if on container
+        await execShellCommand(`docker exec database rm -rf /dump.zip`);
+        // Copy the dump zip file over to the container
+        await execShellCommand(`docker cp ${path.resolve(__dirname, `dump.zip`)} database:/dump.zip`);
+        // Unzip the dump zip into a directory
+        await execShellCommand(`docker exec database unzip /dump.zip -d /`);
+        // Perform the data dump import of the Strapi database
+        await execShellCommand(`docker exec database mongorestore --uri "mongodb://root:capstone@database:27017/?authSource=admin" --drop -d strapi /dump/strapi`);
+        // Log successful execution
+        console.log(`Import successful!`);
+    } catch(e){
+        // Log the error to console
+        console.error(e);
+    }
+})();
