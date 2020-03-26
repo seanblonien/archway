29bbc68c981930ba64337d3b5153f07d6667ec6a
Sean Blonien <sean_blonien@baylor.edu>
2020-03-26 02:53:49 -0500

    Made script output real-time, improved error handling

--- database/export-all.js
+++ database/export-all.js
@@ -3,8 +3,8 @@
  * uploads) and outputs them to their corresponding BaseExports files.
  */
 /* eslint-disable no-console */
-const {execShellCommand} = require('./execShellCommand');
-const {absPath} = require('./execShellCommand');
+const {execShellCommand, parseCmdAndArgs} = require('./execShellCommand');
+const s = require('./strapi-scripts');
 
 // Anonymous method that is directly called to allow for async/await usage
 (async () => {
@@ -12,16 +12,18 @@ const {absPath} = require('./execShellCommand');
   try {
     // Run Strapi export
     console.log('=== Exporting Strapi ===');
-    await execShellCommand(`node ${absPath('export-strapi.js')} ${absPath('BaseStrapiExport.json')}`);
+    await execShellCommand(...parseCmdAndArgs('node export-strapi.js BaseStrapiExport.json'));
     // Run database export
     console.log('=== Exporting Database ===');
-    await execShellCommand(`node ${absPath('export-database.js')} ${absPath('BaseDatabaseExport.zip')}`);
+    await execShellCommand(...parseCmdAndArgs('node export-database.js BaseDatabaseExport.zip'));
     // Run uploads export
     console.log('=== Exporting Uploads ===');
-    await execShellCommand(`node ${absPath('export-uploads.js')} ${absPath('BaseUploadsExport.zip')}`);
+    await execShellCommand(...parseCmdAndArgs('node export-uploads.js BaseUploadsExport.zip'));
 
     console.log('All exports successful!');
+    process.exit(s.successCode);
   }catch (e) {
     console.error(`Error: ${e}`);
+    process.exit(s.errCode);
   }
 })();
--- database/export-strapi.js
+++ database/export-strapi.js
@@ -16,32 +16,38 @@ const ARGV_REQUIRED_LENGTH = 3;
   // Get the export filename to use
   if(process.argv.length < ARGV_REQUIRED_LENGTH) {
     console.error('Please provide a filename for strapi export.');
-    return;
+    process.exit(s.errCode);
   }
   const fileName = process.argv[ARGV_INDEX_FILE_PATH];
 
-  console.log('Logging in to Strapi admin panel...');
-  await s.authenticate();
-  console.log('Login successful!');
+  try {
+    console.log('Logging in to Strapi admin panel...');
+    await s.authenticate();
+    console.log('Login successful!');
 
-  console.log('Fetching all existing content types');
-  const serverContentTypesResponse = await s.axios.get(s.STRAPI_CONTENT_TYPE_URL);
-  // List of the content types on the Strapi server
-  const serverContentTypes = serverContentTypesResponse.data.data.map(t => t.schema);
-  // Remove 'plugin' attribute that cause errors
-  s.removePluginAttribute(serverContentTypes);
-  // Split the content types into 'application' and 'plugins'
-  const serverContentTypesPlugins= serverContentTypes.filter(contentType => contentType.collectionName.startsWith('users-permissions'));
-  const serverContentTypesApplication = serverContentTypes.filter(contentType => !contentType.collectionName.startsWith('users-permissions'));
+    console.log('Fetching all existing content types');
+    const serverContentTypesResponse = await s.axios.get(s.STRAPI_CONTENT_TYPE_URL);
+    // List of the content types on the Strapi server
+    const serverContentTypes = serverContentTypesResponse.data.data.map(t => t.schema);
+    // Remove 'plugin' attribute that cause errors
+    s.removePluginAttribute(serverContentTypes);
+    // Split the content types into 'application' and 'plugins'
+    const serverContentTypesPlugins = serverContentTypes.filter(contentType => contentType.collectionName.startsWith('users-permissions'));
+    const serverContentTypesApplication = serverContentTypes.filter(contentType => !contentType.collectionName.startsWith('users-permissions'));
 
-  // Put the application and plugins content types in a json object
-  const output = {
-    'application': serverContentTypesApplication,
-    'plugins': serverContentTypesPlugins
-  };
+    // Put the application and plugins content types in a json object
+    const output = {
+      'application': serverContentTypesApplication,
+      'plugins': serverContentTypesPlugins
+    };
     // Write that json object to file
-  console.log('Exporting content types to file');
-  s.writeJSONToFile(fileName, output);
+    console.log('Exporting content types to file');
+    s.writeJSONToFile(fileName, output);
 
-  console.log('Strapi schema export successful!!');
+    console.log('Strapi schema export successful!!');
+    process.exit(s.successCode);
+  } catch (e) {
+    console.error(e);
+    process.exit(s.errCode);
+  }
 })();
