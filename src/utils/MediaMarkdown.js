da603308f02367aa4821006055c4221798560295
Sean Blonien <sean_blonien@baylor.edu>
2020-04-02 13:42:37 -0500

    Added width/height query parameter parsing for upload files

--- src/utils/MediaMarkdown.js
+++ src/utils/MediaMarkdown.js
@@ -3,11 +3,32 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {strapiURL} from '../constants';
 
-const MediaMarkdown = ({children}) => (
-  <Markdown>
-    {children.replace('](/uploads/', `](${strapiURL}/uploads/`)}
-  </Markdown>
-);
+const MediaMarkdown = ({children}) => {
+  // Image component will parse a src URL's width/height parameters and size the
+  // image accordingly while stripping off the query parameters
+  const Image = (props) => {
+    const {src, alt} = props;
+    const urlParams = new URLSearchParams((new URL(src)).search);
+    const height = urlParams.get('height');
+    const width = urlParams.get('width');
+    const newSrc = src.split('?')[0];
+
+    return <img {...props} alt={alt} src={newSrc} height={height} width={width}/>;
+  };
+  Image.propTypes = {
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+  };
+
+  // Replaces any relative paths of uploads files to use the strapi url
+  const childrenAbsoluteMedia = children.replace('](/uploads/', `](${strapiURL}/uploads/`);
+
+  return (
+    <Markdown options={{overrides: {img: {component: Image}}}}>
+      {childrenAbsoluteMedia}
+    </Markdown>
+  );
+};
 
 MediaMarkdown.propTypes = {
   children: PropTypes.string.isRequired
