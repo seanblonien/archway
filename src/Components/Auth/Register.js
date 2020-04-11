8aee527bff2c4bdc9e73470eec26c59f2d402b80
Caleb DeHaan <calebdehaan@gmail.com>
2020-04-11 15:59:36 -0500

    added the Register correctly to routes
    haha edited routes.js so we can route Register correctly. Except not fully yet. Pushing so Sean can look @ it :)

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -3,14 +3,13 @@ import Avatar from '@material-ui/core/Avatar';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
 import {makeStyles} from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import React, {useContext, useState} from 'react';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import PersonIcon from '@material-ui/icons/Person';
 import AuthContext from '../../Contexts/AuthContext';
-import routes from '../../utils/Routing/routes';
-import {Link} from '@material-ui/core';
-import {Link as RouterLink} from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -32,14 +31,10 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(1, 0, 1),
   },
-  options: {
-    alignItems:'center'
-  }
 }));
 
-const Login = () => {
-  // TODO: error popup with invalid indentifier/pass
-  const {login} = useContext(AuthContext);
+const Register = () => {
+  const {register} = useContext(AuthContext);
   const [state, setState] = useState({identifier: '', password: '', remember: true});
   const classes = useStyles();
 
@@ -51,18 +46,19 @@ const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const {identifier, password, remember} = state;
-    login(identifier, password, remember);
+    // register(identifier, password, remember);
   };
 
   const {identifier, password, remember} = state;
   return (
+    <Grid item xs={12} sm={8} md={6} xl={6} component={Paper} elevation={6} my={10}>
     <Box alignItems='center' m={5} className={classes.root}>
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
-          <LockOutlinedIcon/>
+          <PersonIcon/>
         </Avatar>
         <Typography component='h1' variant='h5'>
-          Sign in
+          Register as a Sponsor
         </Typography>
         <form className={classes.form} onSubmit={handleSubmit}>
           <Grid container direction='column' spacing={1}>
@@ -110,28 +106,29 @@ const Login = () => {
                 color='primary'
                 className={classes.submit}
               >
-                Sign In
+                Register
               </Button>
             </Grid>
-            <Grid item>
-              <Grid container direction='column' className={classes.options}>
-                <Grid item xs>
-                  <Link variant='body2' component={RouterLink} to={routes.auth.login.path}>
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link variant='body2' component={RouterLink} to={routes.auth.register.path}>
-                    Don't have an account? Sign Up
-                  </Link>
-                </Grid>
-              </Grid>
-            </Grid>
+            {/*<Grid item>*/}
+            {/*  <Grid container direction='column'>*/}
+            {/*    <Grid item xs>*/}
+            {/*      <Link href='/' variant='body2'>*/}
+            {/*        Forgot password? HAHA !*/}
+            {/*      </Link>*/}
+            {/*    </Grid>*/}
+            {/*    <Grid item>*/}
+            {/*      <Link href='/' variant='body2'>*/}
+            {/*        {'Don\'t have an account? Sign Up'}*/}
+            {/*      </Link>*/}
+            {/*    </Grid>*/}
+            {/*  </Grid>*/}
+            {/*</Grid>*/}
           </Grid>
         </form>
       </div>
     </Box>
+    </Grid>
   );
 };
 
-export default Login;
+export default Register;
