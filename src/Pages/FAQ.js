db49ed3b33b33242e1c36c8a13ccd8de632718ca
Sean Blonien <sean_blonien@baylor.edu>
2020-04-27 01:14:31 -0500

    FAQ spacing fixes
    Former-commit-id: 941d37b0653bfa2fe2fc4496963546aad25b59f4

--- src/Pages/FAQ.js
+++ src/Pages/FAQ.js
@@ -1,3 +1,4 @@
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import withStyles from '@material-ui/core/styles/withStyles';
@@ -5,6 +6,8 @@ import Typography from '@material-ui/core/Typography';
 import React, {Component} from 'react';
 import {HashLink as Link} from 'react-router-hash-link';
 import compose from 'recompose/compose';
+import GridPageContainer from '../Components/LayoutWrappers/GridPageContainer';
+import PageWithMargin from '../Components/LayoutWrappers/PageWithMargin';
 import LoadingCircle from '../Components/LoadingCircle';
 import MediaMarkdown from '../Components/Markdown/MediaMarkdown';
 import api from '../Services/api';
@@ -12,18 +15,6 @@ import routes from '../utils/Routing/routes';
 import root from '../utils/styles.module.css';
 
 const styles = theme => ({
-  questions: {
-    marginBottom: '5%',
-    maxWidth: 800
-  },
-  subtitle: {
-    padding: '1%',
-  },
-  content: {
-    padding: 10,
-    marginLeft: '4%',
-    marginBottom: '2%',
-  },
   link: {
     color: theme.palette.primary.main,
     '&:hover': {
@@ -60,61 +51,62 @@ class FAQ extends Component {
 
     if (!loading) {
       return (
-        <div align='center'>
-          <Typography className={root.pageTitle} variant='h2'>Frequently Asked Questions</Typography>
-
-          <Grid container className={classes.questions} spacing={2} direction='row' alignItems='flex-start'>
-            {categories.map((category) => (
-              <Grid key={`top${category.toString()}`} item xs={6}>
-                <Typography variant='h5' align='left'>
-                  {category}
-                </Typography>
-                {faqs.map((question, index) => (
-                  <div key={`top${index}`}>
-                    { category === question.category &&
-                      <Grid item xs={12} style={{marginLeft: '4%'}}>
-
-                        <Link className={classes.link} to={`${routes.faq.path}#question${index}`}>
-                          <Typography align='left'>
-                            {question.question}
-                          </Typography>
-                        </Link>
-                      </Grid>}
-                  </div>
-                ))}
-              </Grid>
-
-            ))}
-          </Grid>
-
-
-          <Grid container alignItems='stretch'>
-
-            {categories.map((category) => (
-              <Paper key={category.toString()} className={root.largePaper}>
-                <Grid item xs={12}>
-                  <Typography variant='h4' align='center' className={classes.subtitle}>
+        <PageWithMargin>
+          <GridPageContainer>
+            <Grid item xs={12}>
+              <Typography variant='h2'>Frequently Asked Questions</Typography>
+            </Grid>
+            <Grid item xs={12} container>
+              {categories.map((category) => (
+                <Grid key={`top${category.toString()}`} item xs={6}>
+                  <Typography variant='h5' align='left'>
                     {category}
                   </Typography>
                   {faqs.map((question, index) => (
-                    <div key={index}>
-                      { category === question.category &&
-                        <Grid item xs={12} style={{marginLeft: '4%'}}>
-                          <Typography variant='h5' id={`question${index}`} align='left'>
-                            {question.question}
-                          </Typography>
-                          <MediaMarkdown className={classes.content} align='left'>
-                            {question.answer}
-                          </MediaMarkdown>
-                        </Grid>}
+                    <div key={`top${index}`}>
+                      {category === question.category &&
+                        <Box item xs={12} style={{marginLeft: '4%'}}>
+                          <Link className={classes.link} to={`${routes.faq.path}#question${index}`}>
+                            <Typography align='left'>
+                              {question.question}
+                            </Typography>
+                          </Link>
+                        </Box>
+                      }
                     </div>
                   ))}
                 </Grid>
-              </Paper>
-            ))}
-          </Grid>
+              ))}
+            </Grid>
 
-        </div>
+            <Grid item xs={12} container>
+              {categories.map((category) => (
+                <Paper key={category.toString()} className={root.largePaper}>
+                  <Grid item xs={12}>
+                    <Typography variant='h4' align='center'>
+                      {category}
+                    </Typography>
+                    {faqs.map((question, index) => (
+                      <div key={index}>
+                        { category === question.category &&
+                          <Box style={{marginLeft: '4%'}}>
+                            <Typography variant='h5' id={`question${index}`}>
+                              {question.question}
+                            </Typography>
+                            <Box textAlign='left'>
+                              <MediaMarkdown>
+                                {question.answer}
+                              </MediaMarkdown>
+                            </Box>
+                          </Box>}
+                      </div>
+                    ))}
+                  </Grid>
+                </Paper>
+              ))}
+            </Grid>
+          </GridPageContainer>
+        </PageWithMargin>
       );
     }
 
